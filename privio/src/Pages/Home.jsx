a78e6a93c81c7440dce40c541ef288a76bfdb75f
SiriChandhana06 <sirisiri3006@gmail.com>
2024-09-06 16:21:34 +0530

    Background

--- privio/src/Pages/Home.jsx
+++ privio/src/Pages/Home.jsx
@@ -4,29 +4,31 @@ import Navbar from '../Components/Navbar';
 import img from '../Assests/secure.png';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
+import Bg from '../Assests/bg.jpg';
 
 
 const Home = () => {
     useEffect(() => {
         Aos.init({ duration: 1000 });
-      }, []);
-  return (
-<div className="container overflow-x-hidden" id='container'>
-    <div>
-        <Navbar/>
-    </div>
-    <div className='pt-20 md:flex justify-between mx-20'data-aos="fade-left" >
-        <div>
-        <h1>Welcome to Privio</h1>
-        <p>Privio is designed to safeguard sensitive personally identifiable information (PII) embedded within documents. It automatically identifies and alerts users to the presence of PII, ensuring compliance with data protection regulations such as GDPR, HIPAA, and CCPA.</p>
+    }, []);
+    return (
+        <div className="relative overflow-x-hidden w-full h-screen bg-cover bg-center" style={{ backgroundImage:` url(${Bg})`}} id='container'>
+            <Navbar />
+            <div className='flex  items-center justify-center h-full text-center px-4 md:px-20 py-10 bg-black bg-opacity-50'>
+                <div data-aos="fade-left" className='text-white font-semibold text-2xl md:text-4xl'>
+                    <h1 className='text-3xl md:text-5xl mb-4'>Welcome to Privio</h1>
+                    <p className='text-lg md:text-xl'>
+                        Privio is designed to safeguard sensitive personally identifiable information (PII) embedded within documents. It automatically identifies and alerts users to the presence of PII, ensuring compliance with data protection regulations such as GDPR, HIPAA, and CCPA.
+                    </p>
+                </div>
+                <div className='mt-8'>
+                    <img src={img} alt='image' className='w-full md:w-1/2 rounded-lg shadow-lg' />
+                </div>
+            </div>
         </div>
-        <div>
-            <img src={img} alt='image'/>
-        </div>
-    </div>
-</div>
 
-  )
+
+    )
 }
 
 export default Home
